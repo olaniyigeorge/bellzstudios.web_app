b470b7bca54bae6bed9b2411eaa294ba0a3d1ef7
Olaniyi George <olaniyigeorge77@gmail.com>
2024-10-12 19:05:47 +0100

    pages

---
+++ app/dev-stories/page.tsx
@@ -0,0 +1,12 @@
+
+
+
+
+export default function Note() {
+
+    return (
+        <div className="w-full h-screen flex justify-center items-center dark:bg-slate-950 text-slate-200"> 
+            Dev stories
+        </div>
+    )
+}
