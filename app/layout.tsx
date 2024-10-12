e00d9ccaf83799dcb36b4d1b6cea3e7dc18fdef3
Olaniyi George <olaniyigeorge77@gmail.com>
2024-10-12 18:55:35 +0100

    metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,8 +22,24 @@ const opensans = localFont({
 export const metadata: Metadata = {
   title: "Bellz Studio",
   description: "Proferring solutions one product at a time",
+  openGraph: {
+    title: "Bellz Studio",
+    description: "Proferring solutions one product at a time",
+    url: "https://bellzstudios.vercel.app/", // "https://www.bellzstudio.com",  // Replace with your actual site URL
+    siteName: "Bellz Studio",
+    images: [
+      {
+        url: "/assets/images/bellzstudio.png",  // Replace with the correct image path
+        width: 1200,
+        height: 630,
+        alt: "Bellz Studio Logo",  // Image description for accessibility
+      },
+    ],
+    type: "website",
+  },
   twitter: {
     card: "summary_large_image",
+    images: "/assets/images/bellzstudio.png",
 },
 };
 
