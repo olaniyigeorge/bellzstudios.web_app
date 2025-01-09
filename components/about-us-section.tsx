278f55b0b1cbd5869556ba88053c31f1e804b6bc
olaniyigeorge <olaniyigeorge77@gmail.com>
2025-01-09 14:50:54 +0100

    fdis

--- components/about-us-section.tsx
+++ components/about-us-section.tsx
@@ -1,4 +1,4 @@
-import { ArrowDownCircleIcon, ArrowDownIcon, ArrowDownOnSquareStackIcon } from "@heroicons/react/24/outline"
+import { ArrowDownIcon } from "@heroicons/react/24/outline"
 import Image from "next/image"
 import Link from "next/link"
 
