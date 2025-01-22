7146baa244d81442569755bd3a4f95a86552a1b5
olaniyigeorge <olaniyigeorge77@gmail.com>
2025-01-22 17:51:42 +0100

    dynamic dev stories metadata

--- app/dev-stories/[id]/page.tsx
+++ app/dev-stories/[id]/page.tsx
@@ -1,33 +1,39 @@
 import { iDevStory } from "@/components/dev-story-card";
 import DevStoryPage from "@/components/dev-story-page";
 import { DEV_STORIES } from "@/components/mock_data"
-import { Metadata } from "next";
 
+export async function generateMetadata( {params }: { params: { id: string } }) {
+  const STORY: iDevStory | undefined = DEV_STORIES.find(story => story.id === params.id);
 
-export const metadata: Metadata = {
-    title: `Dev Stories | Bellz Studios`,
-    description: "Stories behind al the solutions we build",
+  // Check if the story exists to handle any potential errors
+  if (!STORY) {
+      return <div className="w-full">Story not found</div>;
+  }
+  
+  return {
+    title: `${STORY.title} | Dev Stories | Bellz Studios`,
+    description: `${STORY.description.slice(0, 120)}`,
     openGraph: {
-      title: "Dev Stories",
-      description: "Stories behind al the solutions we build",
-      url: "https://Bellz Studio.vercel.app/", // "https://www.bellzstudio.com",  // Replace with your actual site URL
+      title: `${STORY.title}`,
+      description: `${STORY.description.slice(0, 80)}`,
+      url: `https://Bellz Studio.vercel.app/dev-stories/${STORY.title.toLowerCase()}`, // "https://www.bellzstudio.com",  // Replace with your actual site URL
       siteName: "Bellz Studios",
       images: [
         {
-          url: "/assets/images/bellzstudio.png",  // Replace with the correct image path
+          url: `${STORY.image}`,
           width: 1200,
           height: 630,
-          alt: "ibellzstudio",  // Image description for accessibility
+          alt: `${STORY.title}`,  
         },
       ],
       type: "website",
     },
     twitter: {
       card: "summary_large_image",
-      images: "/assets/images/bellzstudio.png",
+      images: `${STORY.image}`,
   },
-  }; 
-
+    }
+  }
   
 export default async function DevStoryyy({ params }: { params: { id: string } }) {
   const STORY: iDevStory | undefined = DEV_STORIES.find(story => story.id === params.id);
