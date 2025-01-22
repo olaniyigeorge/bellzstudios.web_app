dd038e5b65f5cc91ab4e911b7d1cbf69ddfd18ad
olaniyigeorge <olaniyigeorge77@gmail.com>
2025-01-22 17:52:27 +0100

    dynamic dev stories metadata

--- app/dev-stories/page.tsx
+++ app/dev-stories/page.tsx
@@ -1,6 +1,30 @@
 import { DEV_STORIES } from "@/components/mock_data"
 import DevStoryCard, { iDevStory } from "../../components/dev-story-card"
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+    title: `Dev Stories | Bellz Studios`,
+    description: "Stories behind all the solutions and systems we build",
+    openGraph: {
+      title: "Dev Stories",
+      description: "Stories behind all the solutions we build",
+      url: "https://Bellz Studio.vercel.app/", // "https://www.bellzstudio.com",  // Replace with your actual site URL
+      siteName: "Bellz Studios",
+      images: [
+        {
+          url: "/assets/images/bellzstudio.png",  // Replace with the correct image path
+          width: 1200,
+          height: 630,
+          alt: "ibellzstudio",  // Image description for accessibility
+        },
+      ],
+      type: "website",
+    },
+    twitter: {
+      card: "summary_large_image",
+      images: "/assets/images/bellzstudio.png",
+  },
+  }; 
 
 
 
