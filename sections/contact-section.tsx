e25cb9c0b78a913cca09f2e65c8fb2d0ab9217e4
olaniyigeorge <olaniyigeorge77@gmail.com>
2025-01-23 14:26:15 +0100

    rds

--- sections/contact-section.tsx
+++ sections/contact-section.tsx
@@ -1,86 +1,86 @@
-    "use client";
+"use client";
 
-    import ArrowDownIcon from "@heroicons/react/24/outline/ArrowDownIcon";
-    import Image from "next/image";
-    import Link from "next/link";
+import ArrowDownIcon from "@heroicons/react/24/outline/ArrowDownIcon";
+import Image from "next/image";
+import Link from "next/link";
 
-    type iSocial = {
-        name: string;
-        url: string;
-        msg: string;
-        social_logo: string;
-    };
+type iSocial = {
+    name: string;
+    url: string;
+    msg: string;
+    social_logo: string;
+};
 
-    export default function ContactSection() {
-        const contacts: iSocial[] = [
-            {
-                name: "Twitter",
-                url: "https://x.com/imoctborn",
-                msg: "Follow and send a DM",
-                social_logo: "/assets/images/socials/twit.png",
-            },
-            {
-                name: "Email",
-                url: "mailto:olaniyigeorge77@gmail",
-                msg: "Email Me",
-                social_logo: "/assets/images/socials/memoji.png",
-            },
-            {
-                name: "LinkedIn",
-                url: "https://linkedin.com/in/abeleje-olaniyi",
-                msg: "Connect with me",
-                social_logo: "https://avatars.githubusercontent.com/u/27226623?v=4",
-            },
-        ];
+export default function ContactSection() {
+    const contacts: iSocial[] = [
+        {
+            name: "Twitter",
+            url: "https://x.com/imoctborn",
+            msg: "Follow and send a DM",
+            social_logo: "/assets/images/socials/twit.png",
+        },
+        {
+            name: "Email",
+            url: "mailto:olaniyigeorge77@gmail",
+            msg: "Email Me",
+            social_logo: "/assets/images/socials/memoji.png",
+        },
+        {
+            name: "LinkedIn",
+            url: "https://linkedin.com/in/abeleje-olaniyi",
+            msg: "Connect with me",
+            social_logo: "https://avatars.githubusercontent.com/u/27226623?v=4",
+        },
+    ];
 
-        return (
-            <section
-                id="contact"
-                className="page-section bg-white w-full flex flex-col gap-10 md:gap-16 lg:gap-20"
-            >
-                <h1 className=" w-full flex orange-gradient font-bebas-neue-bold uppercase font-extrabold justify-center text-5xl tracking-tighter text-center">
-                    Reach Out To Me
-                </h1>
-                <section className="w-full flex flex-col md:flex-row justify-between items-center gap-3 md:gap-5 lg:gap-8">
-                    {contacts.map((contact) => (
-                        <div
-                            key={contact.name}
-                            className="shadow shadow-slate-600 border-2 border-white w-full bg-black text-white rounded-2xl items-center gap-5 p-3 md:p-5 flex flex-col"
-                        >
-                            <Image
-                                src={contact.social_logo}
-                                alt="contact"
-                                width={200}
-                                height={200}
-                                className="w-24 h-24 object-fill flex justify-center rounded-full"
-                            />
-
-                            <div className="mt-2 flex flex-col items-center">
-                                <span className="orange-gradient font-irishgrover">
-                                    {contact.msg}
-                                </span>
-                                <hr className="" />
-                                <Link
-                                    href={contact.url}
-                                    target="_blank"
-                                    className="font-semibold underline text-white font-bebas-neue-bold"
-                                >
-                                    {contact.name}
-                                </Link>
-                            </div>
-                        </div>
-                    ))}
-                </section>
-
-                <span className="flex gap-2 w-full irishgrover justify-center item-center mt-12">
-                    <Link
-                        href={"#dev-stories"}
-                        className="shadow-lg text-white bg-black border-2 border-orange-500 rounded-full w-fit flex gap-2 font-medium items-center px-4 py-2"
+    return (
+        <section
+            id="contact"
+            className="page-section bg-white w-full flex flex-col gap-10 md:gap-16 lg:gap-20"
+        >
+            <h1 className=" w-full flex orange-gradient font-bebas-neue-bold uppercase font-extrabold justify-center text-5xl tracking-tighter text-center">
+                Reach Out To Me
+            </h1>
+            <section className="w-full flex flex-col md:flex-row justify-between items-center gap-3 md:gap-5 lg:gap-8">
+                {contacts.map((contact) => (
+                    <div
+                        key={contact.name}
+                        className="shadow shadow-slate-600 border-2 border-white w-full bg-black text-white rounded-2xl items-center gap-5 p-3 md:p-5 flex flex-col"
                     >
-                        <>Projects & Dev Stories</>
-                        <ArrowDownIcon className="w-4 h-8 hover:animate-swivvleV text-white font-medium" />
-                    </Link>
-                </span>
+                        <Image
+                            src={contact.social_logo}
+                            alt="contact"
+                            width={200}
+                            height={200}
+                            className="w-24 h-24 object-fill flex justify-center rounded-full"
+                        />
+
+                        <div className="mt-2 flex flex-col items-center">
+                            <span className="orange-gradient font-irishgrover">
+                                {contact.msg}
+                            </span>
+                            <hr className="" />
+                            <Link
+                                href={contact.url}
+                                target="_blank"
+                                className="font-semibold underline text-white font-bebas-neue-bold"
+                            >
+                                {contact.name}
+                            </Link>
+                        </div>
+                    </div>
+                ))}
             </section>
-        );
-    }
+
+            <span className="flex gap-2 w-full irishgrover justify-center item-center text-white  mt-12">
+                <Link
+                    href={"#dev-stories"}
+                    className="shadow-lg bg-black border-2 border-orange-500 rounded-full w-fit flex gap-2 font-medium items-center px-4 py-2"
+                >
+                    <>Projects & Dev Stories</>
+                    <ArrowDownIcon className="w-4 h-8 hover:animate-swivvleV  font-medium" />
+                </Link>
+            </span>
+        </section>
+    );
+}
--- sections/notes-section.tsx
+++ sections/notes-section.tsx
@@ -14,22 +14,24 @@ export default function NotesSection() {
         <section className="w-full flex flex-col font-irishgrover justify-center items-center gap-3">
             <section className="w-full flex text-center justify-center items-center flex-col  gap-4">
                     <span className="text-xl text-center leading-loose w-full md:w-2/3  md:text-2xl text-white text-center font-medium w-full ">
-                        This is a journal for me where I think out loud. Write about my thought 
-                        process when I&apos;m brainstorming, leave notes about what I would build next 
-                        and how I would go about it.
-
+                        This is a journal for me where I think out loud. 
+                        Write about my thought process when I&apos;m 
+                        brainstorming, leave notes about what I would build 
+                        next and how I would go about it.
                     </span>
                     <Link href="/notes" className="flex items-center text-white mt-8 gap-2 hover:scale-[110%] hover:animate-swivvleH transition-all duration-800 ease-in-out shadow shadow-orange-900 p-3 md:p-4 dark:text-black  rounded-full">
-                            <Image 
-                                src="/assets/images/b91e1131ca20f6369aa68d21cb3a8960.gif" 
-                                className="object-full rounded-full w-10 h-10" 
-                                unoptimized
-                                height={200}
-                                width={200}
-                                priority
-                                alt="olaniyigeorge_linkedin"
-                            /> 
-                            <>Read Notes</>
+                        <Image 
+                            src="/assets/images/b91e1131ca20f6369aa68d21cb3a8960.gif" 
+                            className="object-contain rounded-full w-10 h-10" 
+                            unoptimized
+                            height={50}
+                            width={50}
+                            priority
+                            alt="note"
+                        /> 
+                        <>
+                            Read Notes
+                        </>
                     </Link>
             </section>
         </section>
