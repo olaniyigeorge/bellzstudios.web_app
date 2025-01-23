0e1b53592fd26698b6cde45b09717e85f5411d54
olaniyigeorge <olaniyigeorge77@gmail.com>
2025-01-23 08:34:05 +0100

    ... about section

--- app/page.tsx
+++ app/page.tsx
@@ -23,39 +23,39 @@ export default async function Landing() {
 
 
   return (
-    <div className="w-full font-poppins flex flex-col  justify-center">
+    <div className="w-full flex flex-col justify-center">
       
-      <section className="page-section w-full flex flex-col items-center gap-2 py-4 md:py-6">
-            <Typer 
-              id='hero' 
-              delay={50}
-              loop={true}
-              className="font-irishgrover text-[60px] md:text-[160px] text-white font-extrabold " 
-              words={["bellzstudio", "bellzstudio"]}
-            /> 
-            <span
-              id='hero-mini' 
-              className="font-irishgrover text-3xl px-4 md:px-20 text-white orange_gradient text-center" 
-            >
-                Building and contributing to open-source products we believe should exist.
-            </span> 
+      <section className="page-section w-full flex flex-col items-center py-4 md:py-6">
+        <Typer 
+          id='hero' 
+          delay={90}
+          loop={true}
+          className="font-irishgrover text-[60px] md:text-[160px] text-white font-extrabold " 
+          words={["bellzstudio", "bellzstudio"]}
+        /> 
+        <span
+          id='hero-mini' 
+          className="font-irishgrover text-3xl px-4 md:px-20 orange_gradient text-center" 
+        >
+            Building and contributing to open-source products we believe should exist.
+        </span> 
 
-            <div className="w-full flex mt-8 lg:mt-10 gap-3 justify-between items-center" >
-              {links.map((link) => (
-                <ScrollDownAnimation 
-                    key={link.id} 
-                    targetId={link.id} 
-                    className="w-full p-3 md:p-4 flex hover:animate-swivvleV  flex-col  rounded-2xl md:rounded-[50px] border-orange-900 shadow shadow-orange-600 gap-3 shadow shadow-orange-500  items-center"
-                    child={
-                  <>
-                    <span className="text-white text-xs md:text-lg font-irishgrover">{link.name}</span>
-                   
-                    <link.icon className="hover:animate-swivvleV text-orange-500 w-5 h-5"/>
-                  </>
-                } />
-              ))}
-            </div>
-          </section> 
+        <div className="w-full flex my-6 lg:my-8 gap-3 justify-between items-center" >
+          {links.map((link) => (
+            <ScrollDownAnimation 
+                key={link.id} 
+                targetId={link.id} 
+                className="w-full p-3 md:p-4 flex hover:animate-swivvleV  flex-col  rounded-2xl md:rounded-[50px] border-orange-900 shadow shadow-orange-600 gap-3 shadow shadow-orange-500  items-center"
+                child={
+              <>
+                <span className="text-white text-xs md:text-lg font-irishgrover">{link.name}</span>
+                
+                <link.icon className="hover:animate-swivvleV text-orange-500 w-5 h-5"/>
+              </>
+            } />
+          ))}
+        </div>
+      </section> 
 
 
       <AboutUsSection />
